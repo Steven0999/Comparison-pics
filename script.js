9f12ae6d78814bed792c41f33fd6b2d2a8ec43db
Steven0999 <62509586+Steven0999@users.noreply.github.com>
2025-04-26 13:20:57 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example: You can add interactivity if you want
+document.getElementById('photo1').addEventListener('click', function() {
+  alert('You clicked Photo 1!');
+});
+
+document.getElementById('photo2').addEventListener('click', function() {
+  alert('You clicked Photo 2!');
+});
